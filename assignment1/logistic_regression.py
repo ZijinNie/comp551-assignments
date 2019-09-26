49e2da45cff0a3466e445f2880f7d91b0278583d
Zijin Nie <nzjjim@gmail.com>
2019-09-25 21:23:30 -0400

    add test

--- assignment1/logistic_regression.py
+++ assignment1/logistic_regression.py
@@ -6,7 +6,7 @@ Created on Wed Sep 18 19:45:25 2019
 
 import numpy as np
 import math
-import main
+
 class LogisticRegression :
     step = 0
     n_iterations = 0
@@ -15,15 +15,26 @@ class LogisticRegression :
         self.step=step
         self.n_iterations=n_iterations
     
+    def logistic(self,w,Xi):
+        """ Compute the logistic function, given w and the features """
+        # print(w.T)
+        # print(Xi)
+        a = np.dot(w.T,Xi)
+        return 1/(1+np.exp(-a))
+    
+    
     #x: traning data
     #y: traning resut
     #w: parameters
     #m: size of training set
     
     def fit(self, x, y):
+        '''
         n = x.shape[0]
+        x_dummy = np.ones((n,1))
+        w = np.random.normal(0,1,x.shape[1]+1)
+        x= np.concatenate((x, x_dummy),axis=1)
         
-        w = np.random.normal(0,1,x.shape[1])
         
         for _ in range(self.n_iterations):
             
@@ -35,17 +46,32 @@ class LogisticRegression :
                 gradient= np.add(gradient, np.dot(x[i,:],(y[i]- logistic_function)))
                 
             w=np.add(w, (self.step)* gradient)
-        print(w)
+        print(w.shape)
         
         return w 
         #for _ in range(n) : 
          #   log_odds= np.dot(self.w.T,x[i,:])
           #  logistic_function= 1/(1+np.exp(-log_odds))
            # self.error -= (y[i]*math.log(logistic_function)+(1-y[i])*math.log(1-logistic_function))
+        '''
+        n = x.shape[0]
+        m = x.shape[1]
+       
+        w = np.random.rand(m)
+        ak = self.step
+        count = 0 
+        for i in range(m):
+            x[:,i] = np.divide(x[:,i] - np.min(x[:,i]),np.max(x[:,i]) - np.min(x[:,i]))
+        for _ in range(500):
+            der = np.zeros((m))
+            for i in range(n):
+              der += (y[i] - self.logistic(w,x[i,:]))*x[i,:]
+            w = w + ak * der
+            count += 1 
+        return w
         
-        
-    def predict(self, x, w ):
-        n=x.shape[0]
+    def predict(self, x, w):
+        n=x.shape[0]        
         y= np.ones(n)
         for i in range(n):
             log_odds= np.dot(w.T,x[i,:])
@@ -55,37 +81,6 @@ class LogisticRegression :
             else: 
                 y[i] = 0
         return y
-    
-    def k_fold_train_and_validation(self, data, k):
-        size = int(len(data) / k)
-        error = 0
-        for i in range(k):
-            if (i == k-1):
-                validationSet = data[(i * size) : , :]
-                trainSet = data[ : (i * size) , :]
-            else:
-                validationSet = data[(i * size): ((i + 1) * size), :]
-                trainSet = np.concatenate((data[0 : i* size, :], data[(i + 1)* size : , :]),axis=0)
-            x_train= trainSet[: , :-1]
-            y_train= trainSet[:, -1]
-
-            
-            w= self.fit(x_train,y_train)
-            Y_predict = self.predict(validationSet[: , :-1],w)
-            Y_true = validationSet[:, -1]
-            count = 0
-            for j in range(len(Y_true)):
-                if Y_predict[j] != Y_true[j]:
-                    count +=1
-            print("count is{}".format(count ))
-            error += count
-            
-            
-        avg_error = error / k
-        print("# of data is {}".format(data.shape[0]))
-        error_percentage= avg_error / (data.shape[0]/k)
-        print(error_percentage)
-        return error
     
 if __name__ == "__main__":
     
@@ -117,17 +112,76 @@ if __name__ == "__main__":
     '''
     #def kFoldValidation(data,k):
         
+    def seperateByK(data,k):
+        n = data.shape[0]
+        size = math.floor(n/k)
+        segments = []
+        for i in range(k):
+            lower = math.floor( i *size)
+            upper = math.floor( (i+1)*size)
+            segments.append(data[lower:upper, :] )
+        return segments
+    
+    def normalize(data):
+        m = data.shape[1]
+        for i in range(m):
+                data[:,i] = np.divide(data[:,i] - np.min(data[:,i]),np.max(data[:,i]) - np.min(data[:,i]))
+                
+    def k_fold_train_and_validation(data, k, model):
         
+        size = int(len(data) / k)
+        error = 0
+        for i in range(k):
+            if (i == k-1):
+                validationSet = data[(i * size) : , :]
+                trainSet = data[ : (i * size) , :]
+            else:
+                validationSet = data[(i * size): ((i + 1) * size), :]
+                trainSet = np.concatenate((data[0 : i* size, :], data[(i + 1)* size : , :]),axis=0)
+            x_train= trainSet[: , :-1]
+            x_validation = validationSet [:, :-1]
+            y_train= trainSet[:, -1]
+            
+            normalize(x_train)
+            normalize(x_validation)
+          #  for i in range(x_train.shape[1]):
+           #     x_train[:,i] = np.divide(x_train[:,i] - np.min(x_train[:,i]),np.max(x_train[:,i]) - np.min(x_train[:,i]))
+            #    x_validation[:,i] = np.divide(x_validation[:,i] - np.min(x_validation[:,i]),np.max(x_validation[:,i]) - np.min(x_validation[:,i]))
+            w= model.fit(x_train,y_train)
+            Y_predict = model.predict(x_validation,w)
+            Y_true = validationSet[:, -1]
+            count = 0
+            for j in range(len(Y_true)):
+                if Y_predict[j] != Y_true[j]:
+                    count +=1
+            print("count is{}".format(count ))
+            error += count
+            
+        avg_error = error / k
+        print("# of data is {}".format(data.shape[0]))
+        error_percentage= avg_error / (data.shape[0]/k)
+        print(error_percentage)
+        
+        '''
+        w= self.fit(data[:, :-1], data[:, -1])
+        Y_predict = self.predict(data[:, :-1],w)
+        Y_true = data[:, -1]
+        count = 0
+        for j in range(len(Y_true)):
+            if Y_predict[j] != Y_true[j]:
+                count +=1
+        print("count is{}".format(count ))
+        '''
+        return count/ data.shape[0]
     data = genDataWOHeader(file_path)
     qualityToCategory(data)
     testSet, trainSet = seperateTestSet(data)
-    aModel= LogisticRegression(0.01,100)
+    aModel= LogisticRegression(0.01,500)
     print("lr model created, step is {}, n_iteration is {}".format(aModel.step, aModel.n_iterations))
-    trainSet=np.insert(trainSet, trainSet.shape[1]-1,np.ones((trainSet.shape[0],1),dtype=float),axis=1)
+    #trainSet=np.insert(trainSet, trainSet.shape[1]-1,np.ones((trainSet.shape[0],1),dtype=float),axis=1)
     np.random.shuffle(data)
-    data1= main.removeOutLiersByND(data)
     #aModel.fit(x,y)
-    aModel.k_fold_train_and_validation(trainSet, 5)
+    k_fold_train_and_validation(trainSet, 5,aModel)
     
     #print ("model error is {}, model w is {}".format(aModel.error,aModel.w))
         
--- assignment1/main.py
+++ assignment1/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+    
 import logistic_regression as lr
 import numpy as np
 import matplotlib.pyplot as plt
@@ -129,10 +129,10 @@ def featureSelection (model, data):
     return selectedFeatureNum, selectedFeatureArray
         
 
-#normalize data to z-score by each feature except the last column
+#removeOutlier data to z-score by each feature except the last column
 #@Param data: dataset 
 #@return data: the normalized data set
-def normalize(data):
+def removeOutlier(data):
     for i in range(data.shape[1]-1):
         mean = np.mean(data[: , i])
         deviation = np.std(data[: , i])
@@ -145,8 +145,8 @@ def normalize(data):
 #@param trainData: the train data
 #@param k: the number of folds for validation
 #@return accuracy: the accuracy of the model on the given dataset
-def kFoldValidation (model, trainData, k):
-        size = int(len(trainData) / k)
+def LDAKFoldValidation (model, trainData, k):
+        size = math.floor(trainData.shape[0] / k)
         error = 0
         for i in range(k):
             if (i == k-1):
@@ -157,12 +157,53 @@ def kFoldValidation (model, trainData, k):
                 trainSet = np.concatenate((trainData[0 : i* size, :], trainData[(i + 1)* size : , :]),axis=0)
                 
             x_train= trainSet[: , :-1]
+            x_validation=validationSet[: , :-1]
             y_train= trainSet[:, -1]
-            
+            normalize(x_train)
+            normalize(x_validation)
             print(x_train.shape)
             p0, p1, u0, u1, covariance = model.fit(x_train, y_train)
             
-            Y_predict = model.predict(validationSet[: , :-1],u0,u1,p0,p1,covariance)
+            Y_predict = model.predict(x_validation,u0,u1,p0,p1,covariance)
+            Y_true = validationSet[:, -1]
+            count = 0
+            for j in range(Y_true.shape[0]):
+                if Y_predict[j] != Y_true[j]:
+                    count +=1
+            print("count is{}".format(count ))
+            error += count
+            
+        avg_error = error / k
+        accuracy= 1- avg_error / (trainData.shape[0]/k)
+        return accuracy
+
+def normalize(data):
+        m = data.shape[1]
+        for i in range(m):
+                data[:,i] = np.divide(data[:,i] - np.min(data[:,i]),np.max(data[:,i]) - np.min(data[:,i]))
+                
+def LRKFoldValidation(model,data, k) :
+        '''
+        size = math.floor(data.shape[1] / k)
+        error = 0
+        for i in range(k):
+            if (i == k-1):
+                validationSet = data[(i * size) : , :]
+                trainSet = data[ : (i * size) , :]
+            else:
+                validationSet = data[(i * size): ((i + 1) * size), :]
+                trainSet = np.concatenate((data[0 : i* size, :], data[(i + 1)* size : , :]),axis=0)
+            x_train= trainSet[: , :-1]
+            x_validation = validationSet [:, :-1]
+            y_train= trainSet[:, -1]
+            
+            normalize(x_train)
+            normalize(x_validation)
+          #  for i in range(x_train.shape[1]):
+           #     x_train[:,i] = np.divide(x_train[:,i] - np.min(x_train[:,i]),np.max(x_train[:,i]) - np.min(x_train[:,i]))
+            #    x_validation[:,i] = np.divide(x_validation[:,i] - np.min(x_validation[:,i]),np.max(x_validation[:,i]) - np.min(x_validation[:,i]))
+            w= model.fit(x_train,y_train)
+            Y_predict = model.predict(x_validation,w)
             Y_true = validationSet[:, -1]
             count = 0
             for j in range(len(Y_true)):
@@ -172,36 +213,115 @@ def kFoldValidation (model, trainData, k):
             error += count
             
         avg_error = error / k
-        accuracy= 1- avg_error / (trainData.shape[0]/k)
-        return accuracy
- 
+        print("# of data is {}".format(data.shape[0]))
+        error_percentage= avg_error / (data.shape[0]/k)
+        print(error_percentage)
+        
+        return count/ data.shape[0] 
+        '''
+        size = int(len(data) / k)
+        error = 0
+        for i in range(k):
+            if (i == k-1):
+                validationSet = data[(i * size) : , :]
+                trainSet = data[ : (i * size) , :]
+            else:
+                validationSet = data[(i * size): ((i + 1) * size), :]
+                trainSet = np.concatenate((data[0 : i* size, :], data[(i + 1)* size : , :]),axis=0)
+            x_train= trainSet[: , :-1]
+            x_validation = validationSet [:, :-1]
+            y_train= trainSet[:, -1]
+            
+            normalize(x_train)
+            normalize(x_validation)
+          #  for i in range(x_train.shape[1]):
+           #     x_train[:,i] = np.divide(x_train[:,i] - np.min(x_train[:,i]),np.max(x_train[:,i]) - np.min(x_train[:,i]))
+            #    x_validation[:,i] = np.divide(x_validation[:,i] - np.min(x_validation[:,i]),np.max(x_validation[:,i]) - np.min(x_validation[:,i]))
+            w= model.fit(x_train,y_train)
+            Y_predict = model.predict(x_validation,w)
+            Y_true = validationSet[:, -1]
+            count = 0
+            for j in range(len(Y_true)):
+                if Y_predict[j] != Y_true[j]:
+                    count +=1
+            print("count is{}".format(count ))
+            error += count
+            
+        avg_error = error / k
+        error_percentage= avg_error / (data.shape[0]/k)
+        
+        return 1- error_percentage
+    
+    
+def testLRWithWine(a, epochs):
+    data = genDataWOHeader(file_path1)
+    qualityToCategory(data)
+    np.random.shuffle(data)
+    #data1= removeOutLiersByND(data2)
+    testSet, trainSet = seperateTestSet(data)
+    #trainSet=np.insert(trainSet, trainSet.shape[1]-1,np.ones((trainSet.shape[0],1),dtype=float),axis=1)
+    aModel= lr.LogisticRegression(a,epochs)
+    return LRKFoldValidation(aModel,data, 5)
+    
+def testLRWithCancer(a, epochs):
+    data = genData(file_path2)
+    classToCategory(data)
+    np.random.shuffle(data)
+    preprocessData(data)
+
+    #data1= removeOutLiersByND(data2)
+    testSet, trainSet = seperateTestSet(data)
+    #trainSet=np.insert(trainSet, trainSet.shape[1]-1,np.ones((trainSet.shape[0],1),dtype=float),axis=1)
+    aModel= lr.LogisticRegression(a,epochs)
+    return LRKFoldValidation(aModel,data, 5)
+    
+def testLDAWithWine():
+    data = genDataWOHeader(file_path1)
+    qualityToCategory(data)
+    np.random.shuffle(data)
+    #data1= removeOutLiersByND(data2)
+    testSet, trainSet = seperateTestSet(data)
+    aModel = LDA.LDA()
+    return LDAKFoldValidation(aModel,trainSet, 5)
+
+def testLDAWithCancer():
+    data = genData(file_path2)
+    classToCategory(data)
+    preprocessData(data)
+    np.random.shuffle(data)
+    #data1= removeOutLiersByND(data2)
+    testSet, trainSet = seperateTestSet(data)
+    aModel = LDA.LDA()
+    return LDAKFoldValidation(aModel,trainSet, 5)
+    
+#accuracy = testLRWithWine(0.01, 500)
+#print("Accuracy is{}".format(accuracy))
+    
+
 #initialize wine data
 data1 = genDataWOHeader(file_path1)
 qualityToCategory(data1)
-#print(data1)
+np.random.shuffle(data1)
+wineTestSet, wineTrainSet = seperateTestSet(data1)
 
 #initialize cancer data
 data2 = genData(file_path2)
 data2=preprocessData(data2)
 classToCategory(data2)
+np.random.shuffle(data2)
+cancerTestSet, cancerTrainSet = seperateTestSet(data2)
 
-
-np.random.shuffle(data1)
-data1= removeOutLiersByND(data1)
-testSet, trainSet = seperateTestSet(data1)
-trainSet=np.insert(trainSet, trainSet.shape[1]-1,np.ones((trainSet.shape[0],1),dtype=float),axis=1)
-aModel= lr.LogisticRegression(0.01,1000)
-aModel.k_fold_train_and_validation(trainSet,5)
-print("lr model created, step is {}, n_iteration is {}".format(aModel.step, aModel.n_iterations))
-
-
-
-'''
-#shuffle the data
-np.random.shuffle(data1)
 #data1= removeOutLiersByND(data2)
-testSet, trainSet = seperateTestSet(data1)
-aModel = LDA.LDA()
-'''
+
+LRModel= lr.LogisticRegression(0.1,500)
+LDAModel = LDA.LDA()
+#shuffle the data
+#data1= removeOutLiersByND(data2)
+#testSet, trainSet = seperateTestSet(data2)
+#aModel = LDA.LDA()
 #featureSelection(aModel,data1)
-kFoldValidation(aModel,trainSet, 5)
+#LDAKFoldValidation(aModel,trainSet, 5)
+#print(LRKFoldValidation(LRModel, wineTrainSet, 5))
+#print(LDAKFoldValidation(LDAModel, wineTrainSet, 5))
+print(LRKFoldValidation(LRModel, cancerTrainSet, 5))
+print(LDAKFoldValidation(LDAModel, cancerTrainSet, 5))
