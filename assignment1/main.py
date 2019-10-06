8235dd753994fc4a718feb400d77b8726da719c9
Zijin Nie <nzjjim@gmail.com>
2019-10-06 15:24:17 -0400

    update as1

--- assignment1/main.py
+++ assignment1/main.py
@@ -2,9 +2,9 @@
     
 import logistic_regression as lr
 import numpy as np
-import matplotlib.pyplot as plt
 import math
 import LDA
+
 file_path1= "winequality-red.csv"
 file_path2="breast-cancer-wisconsin.data"
 
@@ -13,6 +13,7 @@ def genDataWOHeader (file_path):
     return data
 def genData (file_path):
     data = np.genfromtxt(file_path, delimiter = ",")
+    data = data[:, 1:]
     return data
 
 #categolize the last column of redwine data to 0 and 1
@@ -53,28 +54,34 @@ def preprocessData(data):
     
     return data
             
-# remove the dateset outside 3* standard deviation
+# remove the dateset has 3 features outside 2* standard deviation
 # @Param data: dataset
 #@return: cleaned data
 def removeOutLiersByND (data):
-    x = data[: , :-1]
-    outLierList = np.array([])
-    for i in range(x.shape[1]):
-        mean = np.mean(x[: , i])
-        deviation = np.std(x[: , i])
-        for j in range(x.shape[0]):
-            if x[j, i] >= (mean+ 3*deviation) or x[j, i] <= (mean- 3*deviation):
-                np.append(outLierList, j)
-    np.unique(outLierList)
-    np.delete(data, outLierList, axis= 0)
-    return data
+    mean = []
+    SD = []
+    for i in range(len(data[0]) - 1):
+        mean.append(np.mean(data[:, i]))
+        SD.append(np.std(data[:, i]))
+    
+    delet_arr = np.array([])
+    for i in range(len(data)):
+        count = 0
+        for j in range(len(data[0]) - 1):
+            if abs(data[i, j] - mean[j]) > 2*SD[j]:
+                count += 1
+        if count >= 3:
+            delet_arr = np.append(delet_arr, i)
+    np.unique(delet_arr)
+    clean_data = np.delete(data, delet_arr, axis = 0)
+    return clean_data
 
 #select subset of all features by choosing the best
 #performing subset of all subsets given a model
 #@param model: an instance of a model
 #@Param data: whole dataset
 #@return bestPerformingFeatures : the set of features that performs the best
-def featureSelection (model, data):
+def featureSelection (model, data, isLR):
     selectedFeatureNum = []
     selectedFeatureArray = -1 
     bestAccuracyAll = 0
@@ -84,47 +91,55 @@ def featureSelection (model, data):
         featureToAdd = -1
         bestAccuracy = 0
         column_2d = -1
+        print("select feature{}".format(i))
         if i==0:
-            
             for j in range(data.shape[1]-1):
                 if (j in selectedFeatureNum ) == False:
                     column_2d = np.array([data[:, j]]).T
-                    nums = selectedFeatureNum.append(j)
+                    nums = selectedFeatureNum + [j]
                     
                     # ------5 should be changed --
-                    accuracy = kFoldValidation(model, np.concatenate((column_2d,y_2d), axis = 1),5)
-                    print ("Using feature(s){} accuracy is".format(nums, accuracy))
+                    #print(np.concatenate((column_2d,y_2d), axis = 1))
+                    if isLR:
+                        accuracy = LRKFoldValidation(model, np.concatenate((column_2d,y_2d), axis = 1),5)
+                    else:
+                        accuracy = LDAKFoldValidation(model, np.concatenate((column_2d,y_2d), axis = 1),5)
+
+                    print ("Using feature(s){} accuracy is{}".format(nums, accuracy))
                     if accuracy >= bestAccuracy :
                         bestAccuracy = accuracy
                         featureToAdd = j
             selectedFeatureArray = column_2d 
             bestAccuracyAll = bestAccuracy
             selectedFeatureNum.append(featureToAdd)
-                                      
             continue
         else:   
             #try add feature from the rest of set
             for j in range(data.shape[1]-1):
                 if (j in selectedFeatureNum ) == False:
                     column_2d = np.array([data[:, j]]).T
-                    nums = selectedFeatureNum.append(j)
+                    nums = selectedFeatureNum + [j]
                     
                     # ------5 should be changed ---
-                    print(selectedFeatureArray)
-                    accuracy = kFoldValidation(model, np.concatenate((selectedFeatureArray, column_2d , y_2d), axis = 1),5)
-                    print ("Using feature(s){} accuracy is".format(nums, accuracy))
+                    #print(np.concatenate((selectedFeatureArray, column_2d , y_2d), axis = 1))
+                    if isLR:
+                        accuracy = LRKFoldValidation(model, np.concatenate((selectedFeatureArray, column_2d , y_2d), axis = 1),5)
+                    else:
+                        accuracy = LDAKFoldValidation(model, np.concatenate((selectedFeatureArray, column_2d , y_2d), axis = 1),5)
+                    print ("Using feature(s){} accuracy is{}".format(nums, accuracy))
                     if accuracy >= bestAccuracy :
                         bestAccuracy = accuracy
                         featureToAdd = j
-        #additional feature cannot improve performance
-        if bestAccuracyAll>= bestAccuracy:
+                        
+        #additional feature cannot improve performance by 1%
+        if bestAccuracyAll >= bestAccuracy:
             print ("maxima reached")
             break
         else:
             #add addtional feature
             bestAccuracyAll = bestAccuracy
             selectedFeatureNum.append(featureToAdd)
-            selectedFeatureArray = np.concatenate((selectedFeatureArray,np.array([data[:, j]]).T),axis =1)
+            selectedFeatureArray = np.concatenate((selectedFeatureArray,np.array([data[:, featureToAdd]]).T),axis =1)
     print("feature selection ended, best performing features are {}, the accuracy is {}".format(selectedFeatureNum, bestAccuracyAll))
     return selectedFeatureNum, selectedFeatureArray
         
@@ -159,9 +174,9 @@ def LDAKFoldValidation (model, trainData, k):
             x_train= trainSet[: , :-1]
             x_validation=validationSet[: , :-1]
             y_train= trainSet[:, -1]
-            normalize(x_train)
-            normalize(x_validation)
-            print(x_train.shape)
+            #normalize(x_train)
+            #normalize(x_validation)
+            #print(x_train.shape)
             p0, p1, u0, u1, covariance = model.fit(x_train, y_train)
             
             Y_predict = model.predict(x_validation,u0,u1,p0,p1,covariance)
@@ -170,7 +185,7 @@ def LDAKFoldValidation (model, trainData, k):
             for j in range(Y_true.shape[0]):
                 if Y_predict[j] != Y_true[j]:
                     count +=1
-            print("count is{}".format(count ))
+            #print("count is{}".format(count ))
             error += count
             
         avg_error = error / k
@@ -179,46 +194,11 @@ def LDAKFoldValidation (model, trainData, k):
 
 def normalize(data):
         m = data.shape[1]
-        for i in range(m):
+        for i in range(m-1):
                 data[:,i] = np.divide(data[:,i] - np.min(data[:,i]),np.max(data[:,i]) - np.min(data[:,i]))
-                
+        return data        
 def LRKFoldValidation(model,data, k) :
-        '''
-        size = math.floor(data.shape[1] / k)
-        error = 0
-        for i in range(k):
-            if (i == k-1):
-                validationSet = data[(i * size) : , :]
-                trainSet = data[ : (i * size) , :]
-            else:
-                validationSet = data[(i * size): ((i + 1) * size), :]
-                trainSet = np.concatenate((data[0 : i* size, :], data[(i + 1)* size : , :]),axis=0)
-            x_train= trainSet[: , :-1]
-            x_validation = validationSet [:, :-1]
-            y_train= trainSet[:, -1]
-            
-            normalize(x_train)
-            normalize(x_validation)
-          #  for i in range(x_train.shape[1]):
-           #     x_train[:,i] = np.divide(x_train[:,i] - np.min(x_train[:,i]),np.max(x_train[:,i]) - np.min(x_train[:,i]))
-            #    x_validation[:,i] = np.divide(x_validation[:,i] - np.min(x_validation[:,i]),np.max(x_validation[:,i]) - np.min(x_validation[:,i]))
-            w= model.fit(x_train,y_train)
-            Y_predict = model.predict(x_validation,w)
-            Y_true = validationSet[:, -1]
-            count = 0
-            for j in range(len(Y_true)):
-                if Y_predict[j] != Y_true[j]:
-                    count +=1
-            print("count is{}".format(count ))
-            error += count
-            
-        avg_error = error / k
-        print("# of data is {}".format(data.shape[0]))
-        error_percentage= avg_error / (data.shape[0]/k)
-        print(error_percentage)
-        
-        return count/ data.shape[0] 
-        '''
+
         size = int(len(data) / k)
         error = 0
         for i in range(k):
@@ -232,19 +212,13 @@ def LRKFoldValidation(model,data, k) :
             x_validation = validationSet [:, :-1]
             y_train= trainSet[:, -1]
             
-            normalize(x_train)
-            normalize(x_validation)
-          #  for i in range(x_train.shape[1]):
-           #     x_train[:,i] = np.divide(x_train[:,i] - np.min(x_train[:,i]),np.max(x_train[:,i]) - np.min(x_train[:,i]))
-            #    x_validation[:,i] = np.divide(x_validation[:,i] - np.min(x_validation[:,i]),np.max(x_validation[:,i]) - np.min(x_validation[:,i]))
             w= model.fit(x_train,y_train)
             Y_predict = model.predict(x_validation,w)
             Y_true = validationSet[:, -1]
             count = 0
             for j in range(len(Y_true)):
                 if Y_predict[j] != Y_true[j]:
-                    count +=1
-            print("count is{}".format(count ))
+                    count += 1
             error += count
             
         avg_error = error / k
@@ -252,6 +226,12 @@ def LRKFoldValidation(model,data, k) :
         
         return 1- error_percentage
     
+def addSquareFeature(data, array):
+    for i in array:
+        colToAdd = np.power(data[: , i],3)
+        data = np.insert(data, -1, colToAdd, axis = 1 )
+    return data
+        
     
 def testLRWithWine(a, epochs):
     data = genDataWOHeader(file_path1)
@@ -293,35 +273,8 @@ def testLDAWithCancer():
     testSet, trainSet = seperateTestSet(data)
     aModel = LDA.LDA()
     return LDAKFoldValidation(aModel,trainSet, 5)
-    
+
 #accuracy = testLRWithWine(0.01, 500)
 #print("Accuracy is{}".format(accuracy))
     
 
-#initialize wine data
-data1 = genDataWOHeader(file_path1)
-qualityToCategory(data1)
-np.random.shuffle(data1)
-wineTestSet, wineTrainSet = seperateTestSet(data1)
-
-#initialize cancer data
-data2 = genData(file_path2)
-data2=preprocessData(data2)
-classToCategory(data2)
-np.random.shuffle(data2)
-cancerTestSet, cancerTrainSet = seperateTestSet(data2)
-
-#data1= removeOutLiersByND(data2)
-
-LRModel= lr.LogisticRegression(0.1,500)
-LDAModel = LDA.LDA()
-#shuffle the data
-#data1= removeOutLiersByND(data2)
-#testSet, trainSet = seperateTestSet(data2)
-#aModel = LDA.LDA()
-#featureSelection(aModel,data1)
-#LDAKFoldValidation(aModel,trainSet, 5)
-#print(LRKFoldValidation(LRModel, wineTrainSet, 5))
-#print(LDAKFoldValidation(LDAModel, wineTrainSet, 5))
-print(LRKFoldValidation(LRModel, cancerTrainSet, 5))
-print(LDAKFoldValidation(LDAModel, cancerTrainSet, 5))
