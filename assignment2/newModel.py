bad3c7e3f6bf2e8f1fd0562d6308da7a34351f16
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2019-10-16 23:54:32 -0400

    Models

--- assignment2/newModel.py
+++ assignment2/newModel.py
@@ -4,7 +4,7 @@ import numpy as np
 import string
 import pandas as pd
 from sklearn.datasets import make_classification as mk
-#model
+# model
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import BernoulliNB
@@ -13,28 +13,31 @@ from sklearn.dummy import DummyClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.naive_bayes import MultinomialNB
-from naiveBayes import Berboulli_Naive_Bayes
-from sklearn.ensemble import BaggingClassifier as BC
+from xgboost import plot_importance, XGBClassifier
+from sklearn.preprocessing import OrdinalEncoder
 
-#help_clean
+from naiveBayes import Berboulli_Naive_Bayes
+from sklearn.ensemble import BaggingClassifier as BC, RandomForestClassifier
+
+# help_clean
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
-#help_feature_process
-from sklearn.model_selection import train_test_split
+# help_feature_process
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk import bigrams
-#help_analysis
+# help_analysis
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
-#analysis
+# analysis
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 
+import xgboost as xgb
 
-#read from the file, possible write for testing
 class Reader:
 
     def read(self,path):
@@ -119,9 +122,9 @@ class Cleaner:
         self.words_list = cleaned
 
     def cleaned(self):
-        self.lowercase()
-        #self.remove_punc()
-        self.remove_noncharacter()
+        #self.lowercase()
+        self.remove_punc()
+        #self.remove_noncharacter()
         if self.l:
             self.lemmatizer()
         if self.s:
@@ -154,42 +157,27 @@ class Feature_Processer:
         return X, X_test
 
     def tf_idf(self,X_train,X_test,n_grams,thresold):
-        tf_idf_vectorizer = TfidfVectorizer(ngram_range=n_grams,min_df =thresold)
+        tf_idf_vectorizer = TfidfVectorizer(ngram_range=n_grams,min_df =thresold,binary=True)
         vectors_train_idf = tf_idf_vectorizer.fit_transform(X_train)
         vectors_test_idf = tf_idf_vectorizer.transform(X_test)
         return vectors_train_idf,vectors_test_idf
 
-    def LatentDirichletAllocation(self,X_train,X_test,thresold):
-        v1, v2= self.tf_idf(X_train,X_test,(1,1),thresold)
-        lda = LatentDirichletAllocation(n_components=20)
-        lda.fit(v1)
-        X = lda.transform(v1)
-        X_test1 =lda.transform(v2)
-        return X,X_test1
 
 class classifier:
-    def __init__(self, x_train, x_test, y_train,y_test):
+    def __init__(self, x_train, x_test, y_train, y_test):
         self.x_train = x_train
         self.y_train = y_train
         self.x_test = x_test
         self.y_test = y_test
 
-    def logistic(self, c,epochs):
-        #,max_iter = epochs
-        model = LogisticRegression(C=c, dual=False, solver='lbfgs',multi_class= 'multinomial')
+    def logistic(self, c, epochs):
+        # ,max_iter = epochs
+        model = LogisticRegression(C=c, dual=False, solver='lbfgs', multi_class='multinomial')
         model.fit(self.x_train, self.y_train)
         preds = model.predict(self.x_test)
         scores1 = cross_val_score(model, self.x_train, self.y_train, cv=5, scoring='accuracy')
         print("Score of Logistic in Cross Validation", scores1.mean() * 100)
         print("Losistic Regression : accurancy_matrix is", metrics.accuracy_score(self.y_test, preds))
-        cm = confusion_matrix(self.y_test, preds)
-        #print("Confusion Matrix\n", cm)
-        #print("Report", classification_report(self.y_test, preds))
-
-    def SelfNaiveByes(self):
-        model = Berboulli_Naive_Bayes()
-        model.train(self.x_train,self.y_train)
-        print(model.score(self.x_test,self.y_test))
 
     def Ber_NaiveBayes(self, alpha):
         model = BernoulliNB(alpha=alpha).fit(self.x_train, self.y_train)
@@ -198,8 +186,8 @@ class classifier:
         print("Score of Naive Bayes", scores2.mean() * 100)
         print("Bernoulli Naive Bayes : accurancy_matrix is", metrics.accuracy_score(self.y_test, preds))
         cm = confusion_matrix(self.y_test, preds)
-        #print("Confusion Matrix\n", cm)
-        #print("Report", classification_report(self.y_test, preds))
+        # print("Confusion Matrix\n", cm)
+        # print("Report", classification_report(self.y_test, preds))
 
     def svm(self, c):
         model = LinearSVC(C=c)
@@ -210,75 +198,73 @@ class classifier:
         print("Score of SVM in Cross Validation", scores3.mean() * 100)
         print("SVM Regression : accurancy_is", metrics.accuracy_score(self.y_test, preds))
         cm = confusion_matrix(self.y_test, preds)
-        #print("Confusion Matrix\n", cm)
-        #print("Report", classification_report(self.y_test, preds))
+        # print("Confusion Matrix\n", cm)
+        # print("Report", classification_report(self.y_test, preds))
 
     def decision_tree(self):
-        #criterion=’gini’, splitter=’best’, max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, class_weight=None, presort=False
-        model = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None,min_samples_split=0.1)
-        preds =model.fit(self.x_train, self.y_train)
+        # criterion=’gini’, splitter=’best’, max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, class_weight=None, presort=False
+        model = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=0.1)
+        preds = model.fit(self.x_train, self.y_train)
         scores3 = cross_val_score(model, self.x_train, self.y_train, cv=5, scoring='accuracy')
         print("Score of decision tree in Cross Validation", scores3.mean() * 100)
         print("decision tree  : accurancy_is", metrics.accuracy_score(self.y_test, model.predict(self.x_test)))
         cm = confusion_matrix(self.y_test, preds)
         # print("Confusion Matrix\n", cm)
         # print("Report", classification_report(self.y_test, preds))
-
-    def dummy(self):
-        clf = DummyClassifier(strategy='stratified', random_state=0)
-        clf.fit(self.x_train, self.y_train)
-        score = clf.score(self.x_test, self.y_test)
-        print("Random Baseline's accurancy", score)
+    def random_forest(self):
+        model = RandomForestClassifier
 
     def multNB(self):
         model = MultinomialNB()
         preds = model.fit(self.x_train, self.y_train)
         scores3 = cross_val_score(model, self.x_train, self.y_train, cv=5, scoring='accuracy')
         print("Score of MultinomialNB in Cross Validation", scores3.mean() * 100)
-        print(" MultinomialNB Regression : accurancy_is", metrics.accuracy_score(self.y_test, model.predict(self.x_test)))
+        print(" MultinomialNB Regression : accurancy_is",
+              metrics.accuracy_score(self.y_test, model.predict(self.x_test)))
 
 
-
-def main():
+class main:
     data_raw = Reader().read("reddit_train.csv")
     data_train = data_raw['comments']
     data_test = data_raw['subreddits']
-    #use_lemmer,use_stemmer, use_stopwords
-    cleaner_train = Cleaner(data_train,True,False,False)
+    # use_lemmer,use_stemmer, use_stopwords
+    cleaner_train = Cleaner(data_train, False, False, False)
     cleaner_train.cleaned()
-    
-    X_train, X_test, y_train, y_test = Feature_Processer().split(data_train,data_test,0.9)
-    X_train, X_test = Feature_Processer().tf_idf(X_train, X_test,(1,1),1)
-    #X_train, X_test = Feature_Processer().LatentDirichletAllocation(X_train, X_test,1)
-    #categoryDict = dict([(y,x+1) for x,y in enumerate(sorted(set(y_train)))])
-    #categoryDict = {'AskReddit': 1, 'GlobalOffensive': 2, 'Music': 3, 'Overwatch': 4, 'anime': 5, 'baseball': 6, 'canada': 7, 'conspiracy': 8, 'europe': 9, 'funny': 10, 'gameofthrones': 11, 'hockey': 12, 'leagueoflegends': 13, 'movies': 14, 'nba': 15, 'nfl': 16, 'soccer': 17, 'trees': 18, 'worldnews': 19, 'wow': 20}
-    #y_train = [categoryDict[i] for i in y_train]
-    #y_test = [categoryDict[i] for i in y_test]
-    #X_train, X_test = Feature_Processer().count_vector_features_produce(X_train,X_test,1)
-    #X_train = X_train.toArray()
-    #X_test = X_test.toArray()
-    #clf.SelfNaiveByes()
 
-    clf = classifier(X_train, X_test, y_train, y_test)
-    #a = 0.1 46% 1 53.2% 53% 3 54% 5 10 53% acc 55%, 100 52% 55% ,1000 51% acc 54%
+    X_train, X_test, y_train, y_test = Feature_Processer().split(data_train, data_test, 0.9)
+    X_train, X_test = Feature_Processer().tf_idf(X_train, X_test, (1, 1), 1)
 
+    #clf = classifier(X_train, X_test, y_train, y_test)
+    enc = OrdinalEncoder()
+    X = [['worldnews', 1], ['canada', 2], ['AskReddit', 3],['wow',4],['conspiracy',5],
+         ['nba',6],['leagueoflegends',7],['soccer',8],['funny',9],['movies',10],
+         ['anime',11],['Overwatch',12],['trees',13],['GlobalOffensive',14],['nfl',15],
+        ['europe',16],['Music',20],['baseball',17],['hockey',18],['gameofthrones',19]]
+    enc.fit(X)
+    y_train_matrix= y_train.values.reshape(len(y_train),1)
 
-    #
+    y_test_matrix = y_test.values.reshape(len(y_test), 1)
+    y_train_matrix = enc.transform(y_train_matrix)
+    y_test_matrix=enc.transform(y_test_matrix)
+    print(y_train_matrix)
+    #clf.multNB()
+    # read in data
 
-    #print("Am I start?")
-    #clf.svm(0.2)
-    #svm approximately 56-57% using tf_idf, 54-55% using binary_vetorizor
-    #svm 0.001 42% 0.01 50-51% 0.1 56% 1 55% 10 49
-    #svm 0.1 55.8, 0.2 56.25,0.3 56.17,0.4 55.98,0.5 55.76
+    #model = XGBClassifier()
 
-    #clf.decision_tree()
-    #Decision tree 23% 0.01 28%
-    #Decision tree     0.001 27%
-    #Decision tree     0.1 26%
+    #dtrain = xgb.DMatrix(X_train,y_train_matrix)
+    #dtest = xgb.DMatrix(X_test, y_test_matrix)
+    # specify parameters via map
+    param = {'booster': 'gbtree','max_depth':6,'num_class': 20,'lambda':2, 'eta': 1, 'silent': 1, 'objective': 'multi:softmax'}
+    num_round = 5
+    kf = KFold(n_splits=5, shuffle=True, random_state=1)
+    for train_index, test_index in kf.split(X_train):
+        xgb_model = xgb.XGBClassifier().fit(X_train[train_index], y_train_matrix[train_index])
+        predictions = xgb_model.predict(X_test[test_index])
+        actuals = y_test_matrix[test_index]
+        print(confusion_matrix(actuals, predictions))
 
-    clf.multNB()
-    #multinomial Nb with 55-56% for removing the frequency less than 2 20% 0.0001 54%
-    #bigram with unigram 50% distrucbution 2 51%-52%  0.02 20%
+    #print("bst  : accurancy_is", metrics.accuracy_score(y_test,predictions ))
 
 if __name__ == "__main__":
     main()
